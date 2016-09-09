7e5a9df139ba6d4889a4d4ff9ea272ebf752be92
asterbool <joaob2ol@live.com>
2016-09-09 15:17:13 +0200

    inscription ->acceuil

--- app/Views/default/hello.php
+++ app/Views/default/hello.php
@@ -1,4 +1,4 @@
 <?php $this->layout('layout', ['title' => 'Hello yes !']) ?>
 <?php $this->start('main_content') ?>
-<h1>Hello <?= $username $id ?></h1>
+<h1>bienvenu sur sport and co <?= $firstname. $id; ?></h1>
 <?php $this->stop('main_content') ?>
--- app/config.dist.php
+++ app/config.dist.php
@@ -4,8 +4,8 @@ $w_config = [
    	//information de connexion à la bdd
 	'db_host' => 'localhost',						//hôte (ip, domaine) de la bdd
     'db_user' => 'root',							//nom d'utilisateur pour la bdd
-    'db_pass' => '',								//mot de passe de la bdd
-    'db_name' => '',								//nom de la bdd
+    'db_pass' => 'root',							//mot de passe de la bdd
+    'db_name' => 'movies',							//nom de la bdd
     'db_table_prefix' => '',						//préfixe ajouté aux noms de table
 
 	//authentification, autorisation
