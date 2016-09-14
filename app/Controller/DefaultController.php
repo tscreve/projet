8ff59003073e706082e5f3f1c794f14d6639819f
tscreve <tscreve@hotmail.com>
2016-09-14 16:16:10 +0200

    maj enregistrement annonce

--- app/Controller/DefaultController.php
+++ app/Controller/DefaultController.php
@@ -8,7 +8,6 @@ use \Model\AdvertModel;
 
 class DefaultController extends Controller
 {
-
 	/**
 	 * Page d'accueil par défaut
 	 */
@@ -21,22 +20,32 @@ class DefaultController extends Controller
 	}
 
 
-	public function addPlace(){
+
+	public function addPlace(){		
+		$strDate=date_create_from_format('d/m/Y', $_POST['date']);
+		$date=$strDate->format('Y-m-d');
+		$strTime=date_create_from_format('H', $_POST['time']);
+		$time=$strTime->format('H:i');
+
 		$lat=$_POST['data-lat'];
 		$lng=$_POST['data-lng'];
-
-		$placeStr=$lat.';'.$lng;
-		// var_dump($placeStr);
-
+		
 		$AdvertModel=new AdvertModel();
+		$placeStr=$lat.';'.$lng;
 
 		$advert=array(
-			'place'=>$placeStr
+			'place'=>$placeStr,
+			'description'=>htmlentities($_POST['description']),
+			'level'=>$_POST['level'],
+			'event_date'=>$date,
+			'event_time'=>$time,
+			'nb_participant'=>$_POST['nb_participant'],
+			'statut'=>'available'
 			);
 
 		$AdvertModel->insert($advert);
-
-		$this->show('advert/register');	
+		// $this->show('advert/register');	
+		var_dump($_POST);
 	}
 
 	public function registerPlace(){
