df654a073ad248015e9304462f69551bae7391ac
LaRoachie <laroachie@gmail.com>
2016-09-20 17:23:11 +0200

    Page INSCRIPTION

--- app/Views/user/register_form.php
+++ app/Views/user/register_form.php
@@ -1,49 +1,36 @@
 <?php 
 // On charge le layout
- $this->layout('layout', ['title' => $title, 'message' => $message]);
-
-
+$this->layout('layout_center', ['title' => $title, 'message' => $message]);
 
 // On place le contenu de la vue
-$this->start('droite') ?>
-
- <!-- Modal content -->
-  <div class="modal-content">
-	 <h2 class="titre_inscription">Inscription</h2>
-     <form class="login-form" method="POST" action="<?= $this->url('user_add_user')?>"> 
-
-
-    <label for="gender">Je suis :</label><br>
+$this->start('center') ?>
+<div class="light-box">
+    <form class="register-form" method="POST" action="<?= $this->url('user_add_user')?>"> 
+        <label for="gender">Je suis</label>
         <select name="gender" id="gender">
-          <option value="m" selected="selected">Un homme</option>
-          <option value="f">Une femme</option>         
+            <option value="m" selected="selected">Un homme</option>
+            <option value="f">Une femme</option>         
         </select>
-    <br><br>
 
-      <label for="Prenom">Prenom</label><br>
-      <input placeholder="prenom" type="text" name="firstname" required>
-			<br>
+        <label for="Prenom">Prenom</label>
+        <input type="text" name="firstname" placeholder="prenom" required>
 
-	<label for="date de naissance">Date d'anniversaire :</label><br>
-	<input type="text" id="datepicker" name="birthdate" required></input><br>
-	
-		<br><br>
+    	<label for="date de naissance">Date d'anniversaire :</label>
+    	<input type="text" id="datepicker" name="birthdate" required>
+    	
+        <label for="login">Email</label>
+        <input type="email" name="email" placeholder="Votre Email" >
 
-            <label for="login">Email</label><br>
-            <input placeholder="Votre Email" type="text" name="email"><br><br>
+        <label for="login">Mot de passe</label>
+        <input type="password" name="password" placeholder="Choisissez un mot de passe" >
 
-            <label for="login">Mot de passe</label><br>
-            <input placeholder="Choisissez un mot de passe" type="password" name="password">
-            <br><br>
+        <input type="submit" class="btn btn-warning" value="S'inscrire">   
+    </form>
+</div>
 
-            <input type="submit" value="S'inscrire">   
-        </form>
-  </div>
-
-</div> 
 
 <!-- // fin du contenu de la vue -->
-<?php $this->stop('droite');  ?>
+<?php $this->stop('center');  ?>
 
 
   <?php $this->start('scripts') ?>
